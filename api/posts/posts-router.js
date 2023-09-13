f1ffd66296af96b02aa0c156a1a220fdf46624d9
Michael Gulotta <gulottam@gmail.com>
2023-09-13 15:43:10 -0400

    GET posts passed

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,14 @@
 // implement your posts router here
+const express = require('express')
+const Posts = require('./posts-model')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Posts.find(req)
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+})
+
+module.exports = router
