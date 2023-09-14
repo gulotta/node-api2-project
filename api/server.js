72550c1eb39d4d8153fbdf86c67f90959a7830bc
Michael Gulotta <gulottam@gmail.com>
2023-09-14 14:18:13 -0400

    clean up

--- api/server.js
+++ api/server.js
@@ -1,5 +1,3 @@
-// implement your server here
-// require your posts router and connect it here
 const express = require('express');
 const server = express();
 const postsRouter = require('./posts/posts-router')
